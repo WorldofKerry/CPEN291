a2121d9962602f9594fbf653d3f0fa9521e6936f
Kerry Wang <kerrywang369@gmail.com>
2022-01-25 20:09:48 -0800

    working

--- lab2.py
+++ lab2.py
@@ -4,6 +4,8 @@
 # covid simple dashboard app
 
 # imports
+from ast import Str
+from doctest import master
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from covid import Covid
@@ -21,8 +23,17 @@ def getMasterCovidData() -> list:
         returned by this function
     """
     covid = Covid()  # instantiate
-    data = covid.get_data()
-    return data
+    masterData = covid.get_data()
+
+    # Sudo-random data for active and recovered
+    for data in masterData:
+        if (data['active'] is None):
+            data['active'] = (data['confirmed'] + 2 *
+                              data['deaths'])/2*(data['deaths'] % 7 + 1)
+        if (data['recovered'] is None):
+            data['recovered'] = (2*data['confirmed'] +
+                                 data['deaths'])/2*(data['confirmed'] % 7 + 1)
+    return masterData
 
 
 def getConfirmed(data1: list) -> list:
@@ -36,24 +47,57 @@ def getConfirmed(data1: list) -> list:
     return confirmed
 
 
-def plotConfirmed():
+def getTopCountry(masterData: list, selection: str) -> list:
+    """ this function sues the masterdata selection 
+    and returns a list of (country, value) in [selection] covid cases
+    """
+    return sorted(masterData, key=lambda x: x[selection], reverse=True)
+
+
+def plotTopCountries(selection):
     """ a callback function for the button;
         plots a histogram of the top 10 confirmed cases 
     """
-    global canvas
-    clear()
+    global canvas, masterData, plotted    
     fig = Figure(figsize=(8, 5))
     plot1 = fig.add_subplot(111)
     canvas = FigureCanvasTkAgg(fig, master=window)
-
-    top10 = [confirmed[i] for i in range(10)]
-    # print("DEBUG: top10", top10)
-    x = [top10[i][0] for i in range(10)]
-    y = [top10[i][1] for i in range(10)]
+    top10 = getTopCountry(masterData=masterData, selection=selection)
+    x = [top10[i]['country'] for i in range(10)]
+    y = [top10[i][selection] for i in range(10)]
     plot1.bar(x, y)
 
     for tick in plot1.get_xticklabels():  # rotate the text slightly
         tick.set_rotation(15)
+    clear()
+    canvas.draw()
+    canvas.get_tk_widget().grid(row=1, columnspan=4)
+    plotted = True
+
+
+def plotCountry(selection):
+    """ a callback for the plot country data button"""    
+    data = dict(
+        list(filter(lambda x: x['country'] == selection, countryData))[0])
+    global plotted, canvas
+    fig = Figure(figsize=(8, 5))
+    plot1 = fig.add_subplot(111)
+    canvas = FigureCanvasTkAgg(fig, master=window)
+    # print("DEBUG: data", data)
+    # if (data['active'] is None):
+    #     data['active'] = (data['confirmed'] + 2*data['deaths']
+    #                       )/2*(data['deaths'] % 7 + 1)
+    # if (data['recovered'] is None):
+    #     data['recovered'] = (2*data['confirmed'] +
+    #                          data['deaths'])/2*(data['confirmed'] % 7 + 1)
+
+    x = ["Confirmed", "Active", "Deaths", "Recovered"]
+    y = [data['confirmed'], data['active'], data['deaths'], data['recovered']]
+    plot1.bar(x, y)
+
+    for tick in plot1.get_xticklabels():  # rotate the text slightly
+        tick.set_rotation(15)
+    clear()
     canvas.draw()
     canvas.get_tk_widget().grid(row=1, columnspan=4)
     plotted = True
@@ -65,43 +109,6 @@ def clear():
     if plotted:
         canvas.get_tk_widget().destroy()
         plotted = False
-    global text
-    text.destroy()
-
-
-def plotCountry(selection):
-    """ a callback for the plot country data button"""
-    clear()
-    data = dict(
-        list(filter(lambda x: x['country'] == selection, countryData))[0])
-    # dataString = "Confirmed: " + str(data['confirmed']) + "; Active: " + str(
-    #     data['active']) + "; Deaths: " + str(data['deaths']) + "; Recovered: " + str(data['recovered'])
-    # text = Text(window, width=len(dataString), height=1)
-    # text.insert(INSERT, dataString)
-    # text.grid(row=1, columnspan=3)
-
-    global plotted, canvas
-    if plotted:
-        return
-    fig = Figure(figsize=(8, 5))
-    plot1 = fig.add_subplot(111)
-    canvas = FigureCanvasTkAgg(fig, master=window)
-    # print("DEBUG: data", data)
-    if (data['active'] is None): 
-        data['active'] = (data['confirmed'] + 2*data['deaths'])/2*(data['deaths'] % 7 + 1)
-    if (data['recovered'] is None): 
-        data['recovered'] = (2*data['confirmed'] + data['deaths'])/2*(data['confirmed'] % 7 + 1)
-
-    x = ["Confirmed", "Active", "Deaths", "Recovered"]
-    y = [data['confirmed'], data['active'], data['deaths'], data['recovered']]
-    plot1.bar(x, y)
-
-    for tick in plot1.get_xticklabels():  # rotate the text slightly
-        tick.set_rotation(15)
-    canvas.draw()
-    canvas.get_tk_widget().grid(row=1, columnspan=4)
-    plotted = True
-
 
 
 # program starts here
@@ -119,36 +126,30 @@ window = Tk()
 window.geometry("1000x700")
 
 window.title("Covid Data Visualization")
+window.columnconfigure(4, {'minsize': 40})
 
 plotted = False
 
 plot_label_string = StringVar()
 plot_label_string.set("Plot Top 10 Countries for:")
-plot_label = Label(master=window, textvariable=plot_label_string).grid(row=0, column=0)
+plot_label = Label(master=window, textvariable=plot_label_string).grid(
+    row=0, column=0)
 
-plot_button = Button(master=window,
-                     command=lambda: plotConfirmed(),
-                     height=2,
-                     width=15,
-                     text="Plot").grid(row=0, column=1, sticky=W)
-
-# clear_button = Button(master=window,
-#                       command=lambda: clear(),
-#                       height=2,
-#                       width=10,
-#                       text="Clear").grid(row=0, column=2, sticky='WW')
-
-text = Text()
+options = ['confirmed', 'active', 'deaths', 'recovered']
+variable = StringVar()
+variable.set(options[0])
+listboxCountryNames = OptionMenu(
+    window, variable, *options, command=plotTopCountries).grid(row=0, column=1, sticky=W)
 
 country_label_string = StringVar()
 country_label_string.set("Country's Data:")
-country_label = Label(master=window, textvariable=country_label_string).grid(row=0, column=2, sticky=W)
+country_label = Label(master=window, textvariable=country_label_string).grid(
+    row=0, column=2, sticky=W)
 
 variable = StringVar()
 variable.set(countryNames[0])
 listboxCountryNames = OptionMenu(
     window, variable, *countryNames, command=plotCountry).grid(row=0, column=3, sticky=W)
-plotConfirmed()
-window.mainloop()
 
-# Top Countries for: SelectMenu; Plot Countries: SelectMenu
+plotTopCountries('confirmed')
+window.mainloop()
